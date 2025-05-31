4ec98537fcd8011d46dea254c9c24fcf35a86cc4
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-05-31 13:27:33 +0800

    chore: add redirects
    - Implemented a redirect from the root path (/) to the /dashboard path, ensuring a permanent redirect for improved user navigation.
    - Updated rewrites function to use Promise.resolve for consistency in asynchronous handling.

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  async redirects() {
+    return await Promise.resolve([
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ]);
+  },
   async rewrites() {
-    return [
+    return await Promise.resolve([
       {
         source: "/api/:path*",
         destination: "http://127.0.0.1:5000/api/:path*",
       },
-    ];
+    ]);
   },
 };
 
